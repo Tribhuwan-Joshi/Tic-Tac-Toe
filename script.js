29babed082e0829ae057877befc325340e50f02d
Tribhuwan <mathincosmos@gmail.com>
2022-07-20 14:27:26 +0530

    fix small UI bugs

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Gameboard
 
 const Gameboard = (() => {
@@ -7,188 +6,179 @@ const Gameboard = (() => {
   const setMarkAt = (mark, i) => {
     arr[i] = mark;
     marks++;
-  }
-  const getMarkAt = i => arr[i]
+  };
+  const getMarkAt = (i) => arr[i];
   const getBoard = () => arr;
 
   const isFull = () => marks == 9;
   const reset = () => {
-
     arr = [];
     marks = 0;
-  }
+  };
   return { setMarkAt, getMarkAt, getBoard, isFull, reset };
-
-
 })();
-
-
 
 // Player factory function
 
 const Player = (n, m) => {
-
-
   let mark = m;
   let name = n;
 
-
-
-
   const getMark = () => mark;
   const getName = () => name;
+  const changeName = (n) => name = n;
 
-
-  return { getMark, getName };
-
+  return { getMark, getName, changeName };
 };
-
-
 
 // DisplayController
 
 const displayControl = (() => {
-  const blocks = document.querySelectorAll('[class*=block]');
+  const blocks = document.querySelectorAll("[class*=block]");
 
-  blocks.forEach(b => {
-    b.addEventListener('click', e => setMarkAt(e));
+  blocks.forEach((b) => {
+    b.addEventListener("click", (e) => setMarkAt(e));
   });
   let setMarkAt = (e) => {
     let mark = Game.getCurrentPlayer().getMark();
 
-    let id = e.target.getAttribute('data-id');
+    let id = e.target.getAttribute("data-id");
     if (Game.isGameEnd()) {
-
       return;
     }
     if (!Gameboard.getMarkAt(id)) {
       Gameboard.setMarkAt(mark, id);
 
-
       e.target.textContent = mark;
       Game.checkWinner();
-
     }
-
-
-
-  }
+  };
 
   // points
-  const player1 = document.querySelector('.player1');
+  const player1 = document.querySelector(".player1");
   const player1Count = document.querySelector(".player1-count");
-  const tie = document.querySelector('.tie');
-  const tieCount = document.querySelector('.tie-count');
-  const player2 = document.querySelector('.player2');
+  const tie = document.querySelector(".tie");
+  const tieCount = document.querySelector(".tie-count");
+  const player2 = document.querySelector(".player2");
   const player2Count = document.querySelector(".player2-count");
 
   const toggleCurrent = () => {
     let name = Game.getCurrentPlayer().getName();
-    if (name == 'Player 1') {
-      player1.classList.remove('current');
-      player2.classList.add('current');
-
+    if (name == "Player 1") {
+      player1.classList.remove("current");
+      player2.classList.add("current");
+    } else {
+      player2.classList.remove("current");
+      player1.classList.add("current");
     }
-    else {
-      player2.classList.remove('current');
-      player1.classList.add('current');
-    }
-  }
-
+  };
 
   let reset = () => {
-    blocks.forEach(b => b.textContent = '');
-    restart.style.display = 'none';
+    blocks.forEach((b) => (b.textContent = ""));
+    restart.style.display = "none";
     greet.textContent = "";
-    tie.classList.remove('current');
-    tieCount.classList.remove('current');
-    player1.classList.remove('current');
-    player1Count.classList.remove('current');
-    player2.classList.remove('current');
-    player2Count.classList.remove('current');
+    tie.classList.remove("current");
+    tieCount.classList.remove("current");
+    player1.classList.remove("current");
+    player1Count.classList.remove("current");
+    player2.classList.remove("current");
+    player2Count.classList.remove("current");
     toggleCurrent();
     Game.reset();
     Gameboard.reset();
-
-  }
-
+  };
 
   // Restart
-  let restart = document.querySelector('.restart');
-  let greet = document.querySelector('.restart .greet');
-  let restartBtn = document.querySelector('button#restart-btn');
-  restartBtn.addEventListener('click', reset);
+  let restart = document.querySelector(".restart");
+  let greet = document.querySelector(".restart .greet");
+  let restartBtn = document.querySelector("button#restart-btn");
+  restartBtn.addEventListener("click", reset);
 
   const result = () => {
     let winner = Game.getWinner();
-    restart.style.display = 'flex';
-    if (winner == 'Draw') {
+    restart.style.display = "flex";
+    if (winner == "Draw") {
       greet.textContent = "Draw";
       tieCount.textContent = ++tieCount.textContent;
-      tie.classList.add('current');
-      tieCount.classList.add('current');
+      tie.classList.add("current");
+      tieCount.classList.add("current");
       let winner = Game.getCurrentPlayer().getName();
-      if (winner == 'Player 1')
-        player1.classList.remove('current');
-      else
-        player2.classList.remove('current');
-    }
-
-    else {
+      if (winner == "Player 1") player1.classList.remove("current");
+      else player2.classList.remove("current");
+    } else {
       greet.textContent = `${winner.getName()} win`;
-      if (winner.getName() == 'Player 1') {
+      if (winner.getName() == "Player 1") {
         player1Count.textContent = ++player1Count.textContent;
-        player1.classList.add('current');
-        player1Count.classList.add('current');
-      }
-
-      else {
+        player1.classList.add("current");
+        player1Count.classList.add("current");
+      } else {
         player2Count.textContent = ++player2Count.textContent;
-        player2.classList.add('current');
-        player2Count.classList.add('current');
+        player2.classList.add("current");
+        player2Count.classList.add("current");
       }
     }
+  };
+
+  // change Mode
+
+  function changeMode(m) {
+    if (m == "AI-N" || m == "AI-H") {
+      player2.textContent = "Computer - O";
+    }
+    else {
+      player2.textContent = "Player 2 - O";
+    }
+    blocks.forEach((b) => (b.textContent = ""));
+    let currentPlayer = Game.getCurrentPlayer().getName();
+    if (currentPlayer == "Player 1") {
+      player2.classList.remove("current");
+    }
+    else {
+      player1.classList.remove("current");
+    }
+    restart.style.display = "none";
+    greet.textContent = "";
+    tie.classList.remove("current");
+    tieCount.classList.remove("current");
+    player1Count.textContent = "0";
+    player2Count.textContent = "0";
+    player1Count.classList.remove("current");
+    player2Count.classList.remove("current");
 
   }
 
-
   // mode - driver
 
-  const mode_img = document.querySelector('.mode-img img');
-  const mode_name = document.querySelector('.mode-name');
-  const mode_data = document.querySelector('.mode-data');
+  const mode_img = document.querySelector(".mode-img img");
+  const mode_name = document.querySelector(".mode-name");
+  const mode_data = document.querySelector(".mode-data");
   let i = 0;
-  mode_img.addEventListener('click', () => {
+  // mode_img.addEventListener("click", () => {
+  //   let states = ["2P", "AI-N", "AI-H"];
+  //   let url = ["./icons/2p.png", "./icons/AIN.png", "./icons/AIH.png"];
 
-
-    let states = ["2P", "AI-N", "AI-H"];
-    let url = ["./icons/2p.png", "./icons/AIN.png", "./icons/AIH.png"];
-
-    return function () {
-
-      i = ++i % states.length;
-      mode_img.setAttribute('src', url[i]);
-      mode_name.textContent = states[i];
-
-    }()
-  });
+  //   return (function () {
+  //     i = ++i % states.length;
+  //     mode_img.setAttribute("src", url[i]);
+  //     mode_name.textContent = states[i];
+  //     changeMode(states[i]);
+  //     Game.changeMode(states[i]);
+  //     Gameboard.reset();
+  //   })();
+  // });
 
   return { reset, setMarkAt, result, toggleCurrent };
-
-
 })();
 
-
-
-// Game 
+// Game
 
 const Game = (() => {
-  let p1 = Player('Player 1', 'X');
-  let p2 = Player('Player 2', 'O');
-  let currentPlayer = p1;
-
   let winner = null;
   let gameEnd = false;
+  let mode = "2P";
+  let p1 = Player("Player 1", "X");
+  let p2 = Player("Player 2", "O");
+  let currentPlayer = p1;
 
 
   const getWinner = () => winner;
@@ -197,22 +187,28 @@ const Game = (() => {
   const toggleCurrentPlayer = () => {
     if (currentPlayer == p1) {
       currentPlayer = p2;
-
-    }
-    else
-
-      currentPlayer = p1;
-
+    } else currentPlayer = p1;
   };
   const reset = () => {
-    winner = '';
+    winner = "";
     gameEnd = false;
     toggleCurrentPlayer();
+  };
+
+  const changeMode = (m) => {
+    mode = m;
+    gameEnd = false;
+    winner = null;
+
+    if (m == "2P")
+      p2.changeName("Player 2");
+    else
+      p2.changeName("Computer");
+
 
   }
 
   const checkWinner = () => {
-
     let arr = Gameboard.getBoard();
     if (
       (arr[0] == "X" && arr[1] == "X" && arr[2] == "X") ||
@@ -229,10 +225,7 @@ const Game = (() => {
 
       gameEnd = true;
       displayControl.result();
-
-
-    }
-    else if (
+    } else if (
       (arr[0] == "O" && arr[1] == "O" && arr[2] == "O") ||
       (arr[0] == "O" && arr[3] == "O" && arr[6] == "O") ||
       (arr[2] == "O" && arr[4] == "O" && arr[6] == "O") ||
@@ -247,35 +240,23 @@ const Game = (() => {
 
       gameEnd = true;
       displayControl.result();
-    }
-    else if (Gameboard.isFull()) {
-      winner = 'Draw';
+    } else if (Gameboard.isFull()) {
+      winner = "Draw";
       gameEnd = true;
       displayControl.result();
-
-    }
-    else {
-
+    } else {
       displayControl.toggleCurrent();
       toggleCurrentPlayer();
     }
+  };
 
-  }
-
-  return { getCurrentPlayer, toggleCurrentPlayer, checkWinner, getWinner, isGameEnd, reset };
-
-
+  return {
+    getCurrentPlayer,
+    toggleCurrentPlayer,
+    checkWinner,
+    getWinner,
+    isGameEnd,
+    reset,
+    changeMode
+  };
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
